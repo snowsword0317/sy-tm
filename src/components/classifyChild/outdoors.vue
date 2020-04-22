81e2020d56fec356913095569ec8d6a305c97fb5
snowsword <3068054878@qq.com>
2020-04-22 20:58:29 +0800

    所有子组件已布置完毕，需要明天再来进行css编码，连接数据

--- src/components/classifyChild/outdoors.vue
+++ src/components/classifyChild/outdoors.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-    name:"wWear"
+    name:"outdoors"
 }
 </script>
 
---
+++ src/components/classifyChild/wShoes.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        为您推荐
+    </div>
+</template>
+
+<script>
+export default {
+    name:"wShoes"
+}
+</script>
+
+<style scoped>
+
+</style>
