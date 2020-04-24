e78fc8461c467c6fed032f2f53fffc9a62038687
snowsword <3068054878@qq.com>
2020-04-24 09:19:55 +0800

    分类子模块的壳子诞生

--- src/components/classifyChild/recommended.vue
+++ src/components/classifyChild/recommended.vue
@@ -1,20 +1,40 @@
 <template>
-    <div>
-        <h2 class="first-t"></h2>
-    </div>
+  <div>
+    <h2 class="first-t">女装</h2>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"recommended"
-}
+  name: "recommended"
+};
 </script>
 
 <style scoped>
-.first-t{
-    font-size: 14px;
-    line-height: 14px;
-    color: #9B9B9B;
-    padding: 15px 0;
+.first-t {
+  font-size: 14px;
+  line-height: 14px;
+  color: #9b9b9b;
+  padding: 20px 10px;
+}
+ul {
+  display: grid;
+  grid-template-columns: 76px 76px 76px;
+  list-style-type: none;
+  border: 1px solid seagreen;
+  padding: 0px 10px;
+}
+li {
+  border: 1px solid salmon;
+  width: 76px;
+  height: 89px;
 }
 </style>
