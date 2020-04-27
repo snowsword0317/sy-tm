61145fc549f34f02a7d9a4270e297d6c8c64af0b
snowsword <3068054878@qq.com>
2020-04-27 18:42:35 +0800

    补充完所有的分类内容，分类子组件已不在关注内，下一步，主页或者搜索栏;)

--- src/components/classifyChild/flowerPet.vue
+++ src/components/classifyChild/flowerPet.vue
@@ -1,15 +1,469 @@
 <template>
-    <div>
-        为您推荐
+  <div>
+    <div class="channel">
+      <a href>进入天猫国际频道 &gt;</a>
     </div>
+    <h2 class="first-t">女装</h2>
+    <ul>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50025145&from=nav_ct_176_8141_8142&spm=a223c.7814651.8141nav.0"
+        >
+          <div>
+            <img
+              src="https://img.alicdn.com/tfs/TB1YTCkqXOWBuNjy0FiXXXFxVXa-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>连衣裙</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50029627&from=nav_ct_176_8141_8143&spm=a223c.7814651.8141nav.1"
+        >
+          <div>
+            <img
+              src="https://img.alicdn.com/tfs/TB1I0W2qf1TBuNjy0FjXXajyXXa-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>T恤</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1XzKuqXuWBuNjSspnXXX1NVXa-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>衬衫</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50025145&from=nav_ct_176_8141_8142&spm=a223c.7814651.8141nav.0"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1JjmnqkOWBuNjSsppXXXPgpXa-100-100.jpg_170x120Q50s50.jpg_.webp"
+              alt
+            />
+          </div>
+          <p>雪纺衫</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50025145&from=nav_ct_176_8141_8142&spm=a223c.7814651.8141nav.0"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1kO5nqkOWBuNjSsppXXXPgpXa-100-100.jpg_170x120Q50s50.jpg_.webp"
+              alt
+            />
+          </div>
+          <p>裤子</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50025145&from=nav_ct_176_8141_8142&spm=a223c.7814651.8141nav.0"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1H6enqkOWBuNjSsppXXXPgpXa-100-100.jpg_170x120Q50s50.jpg_.webp"
+              alt
+            />
+          </div>
+          <p>牛仔裤</p>
+        </a>
+      </li>
+    </ul>
+    <h2 class="first-t">女鞋</h2>
+    <ul>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50025145&from=nav_ct_176_8141_8142&spm=a223c.7814651.8141nav.0"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1gh88SVXXXXciaXXXXXXXXXXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>小白鞋</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50029627&from=nav_ct_176_8141_8143&spm=a223c.7814651.8141nav.1"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1yNqkSVXXXXXBXVXXXXXXXXXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>乐福鞋</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1lwyrSVXXXXbCXFXXXXXXXXXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>草编鞋</p>
+        </a>
+      </li>
+    </ul>
+    <h2 class="first-t">男装</h2>
+    <ul>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50025145&from=nav_ct_176_8141_8142&spm=a223c.7814651.8141nav.0"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1h.PxqmBYBeNjy0FeXXbnmFXa-100-100.png_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>T恤</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50029627&from=nav_ct_176_8141_8143&spm=a223c.7814651.8141nav.1"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1faWZqhGYBuNjy0FnXXX5lpXa-100-100.png_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>衬衫</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1EMREqbSYBuNjSspiXXXNzpXa-100-100.png_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>POLO衫</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1vUWzqkCWBuNjy0FaXXXUlXXa-100-100.png_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>夹克</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB1qW4YqbGYBuNjy0FoXXciBFXa-100-100.png_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>休闲裤</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tfs/TB11StUqXGWBuNjy0FbXXb4sXXa-100-100.png_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>牛仔裤</p>
+        </a>
+      </li>
+    </ul>
+    <h2 class="first-t">家电</h2>
+    <ul>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50025145&from=nav_ct_176_8141_8142&spm=a223c.7814651.8141nav.0"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tps/i1/TB1IQsKGVXXXXXLXpXXMxXJVFXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>平板电视</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50029627&from=nav_ct_176_8141_8143&spm=a223c.7814651.8141nav.1"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tps/i4/TB1TcZzGVXXXXc_apXXMxXJVFXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>洗衣机</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tps/i3/TB19_gEGVXXXXbAXVXXMxXJVFXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>冰箱</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tps/i2/TB18cUQGVXXXXanXXXXMxXJVFXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>电饭煲</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tps/i4/TB1do7HGVXXXXaoXVXXMxXJVFXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>空气净化器</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tps/i3/TB1CjwPGVXXXXa1XXXXMxXJVFXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>扫地机器人</p>
+        </a>
+      </li>
+    </ul>
+    <h2 class="first-t">美妆</h2>
+    <ul>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50025145&from=nav_ct_176_8141_8142&spm=a223c.7814651.8141nav.0"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tps/i4/TB1ZsMAGVXXXXcMaXXXMxXJVFXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>护肤着装</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=50029627&from=nav_ct_176_8141_8143&spm=a223c.7814651.8141nav.1"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tps/i1/TB1IDECGVXXXXXOXVXXMxXJVFXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>面膜</p>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://s.m.tmall.com/search.htm?cat=162104&from=nav_ct_176_8141_8145&spm=a223c.7814651.8141nav.2"
+        >
+          <div>
+            <img
+              src="//img.alicdn.com/tps/i4/TB19M7HGVXXXXaGXpXXMxXJVFXX-100-100.jpg_170x120Q50s50.jpg_.webp"
+            />
+          </div>
+          <p>身体护理</p>
+        </a>
+      </li>
+    </ul>
+    <h2 class="first-t">品牌墙</h2>
+     <ul class="spoce">
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=107922698&spm=a223c.7814651.142nav.0"
+        >
+          <img
+            src="//img.alicdn.com/tps/i4/TB1PhrmHpXXXXX0XXXXXD.cNVXX-400-200.png_170x120Q90s50.jpg_.webp"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=109681130&spm=a223c.7814651.142nav.1"
+        >
+          <img
+            src="//img.alicdn.com/tps/i4/TB11KlzHXXXXXcKXXXXwu0bFXXX.png_170x120Q50s50.jpg_.webp"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=63098095&spm=a223c.7814651.142nav.2"
+        >
+          <img src="//img.alicdn.com/tps/i4/T1bH9CXmhfXXb1upjX.jpg_170x120Q50s50.jpg_.webp" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=102435051&spm=a223c.7814651.142nav.3"
+        >
+          <img
+            src="//img.alicdn.com/tps/i4/TB14PoAGVXXXXXXaXXXwu0bFXXX.png_170x120Q50s50.jpg_.webp"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=111852592&spm=a223c.7814651.142nav.4"
+        >
+          <img src="//img.alicdn.com/tps/i4/T1QnFTXXpFXXb1upjX.jpg_170x120Q50s50.jpg_.webp" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=63098095&spm=a223c.7814651.142nav.2"
+        >
+          <img
+            src="//img.alicdn.com/tps/i4/TB1MWWGGVXXXXbeaXXXSutbFXXX.jpg_170x120Q50s50.jpg_.webp"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=107922698&spm=a223c.7814651.142nav.0"
+        >
+          <img
+            src="//img.alicdn.com/tps/i4/T1OnjUXdBaXXXQXDnq-90-45.png_170x120Q50s50.jpg_.webp"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=109681130&spm=a223c.7814651.142nav.1"
+        >
+          <img
+            src="//img.alicdn.com/tps/i4/T1h6otXftXXXb1upjX_170x120Q50s50.jpg_.webp"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=63098095&spm=a223c.7814651.142nav.2"
+        >
+          <img src="//img.alicdn.com/tps/i4/TB1W423GFXXXXa_aXXXwu0bFXXX.png_170x120Q50s50.jpg_.webp" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://shop.m.taobao.com/shop/shopIndex.htm?shop_id=102435051&spm=a223c.7814651.142nav.3"
+        >
+          <img
+            src="//img.alicdn.com/tps/i4/TB1C6jpHpXXXXbrXFXXwu0bFXXX.png_170x120Q50s50.jpg_.webp"
+          />
+        </a>
+      </li>
+     </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"flowerPet"
-}
+  name: "flowerPet"
+};
 </script>
 
 <style scoped>
+.first-t {
+  font-size: 14px;
+  line-height: 14px;
+  color: #9b9b9b;
+  padding: 20px 10px;
+}
+ul {
+  display: grid;
+  grid-template-columns: 34% 34% 34%;
+  list-style-type: none;
+  padding: 0px 10px;
+}
+li {
+  height: 89px;
+}
+a {
+  text-decoration: none;
+}
+img {
+  width: 58px;
+  height: 58px;
+  margin: 0 8px;
+}
+p {
+  line-height: 23px;
+  height: 23px;
+  font-size: 14px;
+  color: #5d5d5d;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.channel{
+    width: 95%;
+    height: 35px;
+    background: rgb(255, 0, 54);
+    line-height: 35px;
+    text-align: center;
+    margin: 6% 2.5%;
+}
+.channel>a{
+    font-size: 12px;
+    color: #fff;
+}
 
+.spoce{
+  display: grid;
+  grid-template-columns: 34% 34% 34%;
+  list-style-type: none;
+  padding: 0px 10px;
+}
+.spoce li {
+  height: 49px;
+  padding: 8px;
+}
+.spoce img {
+  width: 58px;
+  height: 29px;
+}
 </style>
